068174ee49b5257e8fed6ed6b74d97206d67f836
masifkiborg <gerroma850@gmail.com>
2025-11-04 21:34:47 +0300

    api calling fix

--- bot/bot.py
+++ bot/bot.py
@@ -11,6 +11,16 @@ class TgBot:
         self.application.add_handler(CommandHandler("start",start_command))
         self.application.add_handler(CallbackQueryHandler(button_handler))
 
+    def _setup_jobs(self):
+        job_queue = self.application.job_queue
+        job_queue.run_repeating(self._keep_alive, interval=50,first=10)
+
+    async def _keep_alive(self,context):
+        try:
+            bot_info = await self.application.bot.get_me()
+        except Exception as e:
+            print()
+
     def run(self):
         print("START")
         self.application.run_polling()
